a6611d5dcaa97031af13185c239f60041cf04f6f
rue hyder <ruehyder@outlook.com>
2020-09-19 05:25:14 -0700

    create application assignment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', "text/plain");
-  response.end('Hello CSC 3221 World, you are the best');
+  response.end('Welcome to my Web Application,\n\nMy name is: Rue');
 
 });
 
